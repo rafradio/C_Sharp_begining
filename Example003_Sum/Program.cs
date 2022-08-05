68951ee897ef32e403cabbfc16886e2630257ac5
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-05 14:36:08 +0300

    Adding random for sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // От 1 до 9
+int numberB = new Random().Next(1, 10);
 int output = numberA + numberB;
+Console.Write("Первое число = " + numberA + "  ");
+Console.WriteLine("Второе число = " + numberB);
 Console.WriteLine("Сумма равна = " + output);
