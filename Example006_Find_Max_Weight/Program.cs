962505434f86719a63837d3cdb28cdbd14aa21e0
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-05 15:32:56 +0300

    Find max weight - Second  attempt (Loop for)

--- Example006_Find_Max_Weight/Program.cs
+++ Example006_Find_Max_Weight/Program.cs
@@ -1,3 +1,7 @@
-﻿int[] weight = {1, 2, 6, 8, 4};
-int max = weight[2];
-Console.WriteLine("Третье число - " + max);
+﻿int[] weight = {1, 2, 16, 8, 4};
+int max = weight[0];
+for (int i = 0; i < weight.Length; i++) 
+{
+  if (max < weight[i]) max = weight[i];
+}
+Console.WriteLine("Максимальный вес - " + max);
